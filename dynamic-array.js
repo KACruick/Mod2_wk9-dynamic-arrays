38133f57ccc33abdb1626e17e58bce20b4a4e5ab
Kendra <cruickshank.kendra@gmail.com>
2024-05-11 13:58:25 -0700

    finished

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,78 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = Array(this.capacity)
+    
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index]
+    
   }
 
   push(val) {
-
-    // Your code here 
+    if (this.length === this.capacity){
+      this.resize()
+    }
+    this.data[this.length] = val
+    this.length++
   }
 
 
   pop() {
-
-    // Your code here 
+    if (this.length === 0) {
+      return undefined
+    }
+    let num = this.data[this.length-1]
+    this.data[this.length-1] = undefined
+    this.length--
+    return num;
   }
 
   shift() {
+    let num = this.data[0]
 
-    // Your code here 
+    if (this.length === 0){
+      return undefined
+    }
+
+    for(let i = 0; i < this.length-1; i++){
+      this.data[i] = this.data[i+1]
+      
+    }
+    this.length--
+    return num
   }
 
   unshift(val) {
-
-    // Your code here 
+    if (this.length === this.capacity){
+      this.resize()
+    }
+    for(let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i-1]
+    }
+    this.length++;
+    this.data[0] = val
   }
 
   indexOf(val) {
-
-    // Your code here 
+  for (let i = 0; i < this.length; i++){
+    if (this.data[i] === val){
+      return i
+    } 
+  }
+  return -1
   }
 
   resize() {
-
-    // Your code here 
+    this.capacity *= 2
+    const newData = new Array(this.capacity)
+    for(let i = 0; i < this.length; i++){
+      newData[i] = this.data[i]
+    }
+    this.data = newData
   }
 
 }
